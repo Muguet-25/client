e411bb4c1c8d4ff2c94723a0536e36a51e7a5a8e
김우찬 <dnckss1121@gmail.com>
2025-09-25 19:07:14 +0900

    fix :: dashboard

--- components/dashboard/StatsCard.tsx
+++ components/dashboard/StatsCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+
 interface StatsCardProps {
   title: string;
   value: string;
@@ -12,33 +13,33 @@ export default function StatsCard({ title, value, changePercent, changeType, ico
   return (
     <div className="bg-[#1c1c28] border border-[#3a3b50] rounded-[20px] p-6 h-full">
       <div className="space-y-10">
-        <div className="flex flex-col space-y-6">
+        <div className="flex flex-col space-y-1">
           <h3 className="text-white text-base font-normal">{title}</h3>
-          
+
           <div className="space-y-6">
             <div className="text-[#e2e2e4] text-5xl font-bold leading-[54px]">
               {value}
             </div>
-            
-            {/*
-              <div className={`inline-flex items-center gap-1 px-2 py-1 rounded-md text-sm ${
-              changeType === 'increase' 
-                ? 'bg-[#ff8953] bg-opacity-40 text-[#ff8953]' 
-                : 'bg-[#0065ff] bg-opacity-40 text-[#0065ff]'
-            }`}>
-            */}
-            {/*
+
+
+            <div className={`inline-flex items-center gap-1 px-2 py-1 rounded-md text-sm ${changeType === 'increase'
+                ? 'bg-[#ff8953]/80  text-white'
+                : 'bg-[#0065ff]/80 text-white'
+              }`}>
+
+
               <span className="material-symbols-rounded text-lg">
-                {changeType === 'increase' ? 'stat_1' : 'keyboard_arrow_down'}
+                {changeType === 'increase' ? '' : '' }
               </span>
-            */}
-              {/*
-                <span>{changePercent}</span>
-              */}
+              <span>{changePercent}</span>
+              </div>
+
               
+
+
             </div>
           </div>
         </div>
       </div>
-  );
+      );
 }
